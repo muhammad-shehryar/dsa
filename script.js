fde84cd09e1b43f7f1b78ea4dcee95a2ca0d3731
shaheryar <muhammadshaheryar50@gmail.com>
2024-10-19 16:12:09 +0500

    change

--- script.js
+++ script.js
@@ -60,3 +60,25 @@ function moveNonZeroToEnd(arr){
 }
 
 console.log(moveNonZeroToEnd(arr3))
+
+function binarySearch(arr,target){
+    let start = 0;
+    let end = arr.length -1;
+
+    while(start<=end){
+        let mid = Math.floor((start+end)/2)
+        if(arr[mid] === target){
+            return mid;
+        }
+        else if(arr[mid] < target){
+            start=mid+1
+        }
+        else if(arr[mid]> target){
+            end = mid -1
+        }
+        
+    }
+    return -1;
+}
+let arr4 = [1, 3, 5, 7, 9, 11];
+let target = 7;
